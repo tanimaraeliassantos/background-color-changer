947a5d97bcc5228ee8e5be48626ab7e48487b9c7
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2024-07-03 13:00:53 +0200

    Update script.js
    Creates array of color to randomize background
    Creates function to get a random color from array

---
+++ script.js
@@ -0,0 +1,18 @@
+const ColorsArr = [
+	'#A200FF',
+	'#1BA1E2',
+	'#E51400',
+	'#FFC425',
+	'#4A235A',
+	'#D11141',
+	'#00B159',
+	'#00AEDB',
+	'#F37735',
+	'#800020',
+];
+
+function getRandomIndex() {
+	console.log(ColorsArr.length * Math.random());
+}
+
+getRandomIndex();
