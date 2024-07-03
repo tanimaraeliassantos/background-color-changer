54713d9210ae72bd53d0a8a7d142f04ade396bd0
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2024-07-03 13:12:56 +0200

    Update script.js
    Removes logging

--- script.js
+++ script.js
@@ -20,4 +20,3 @@ function getRandomIndex() {
 const body = document.querySelector('body');
 const bgHexCodeSpanElement = document.querySelector('#bg-hex.code');
 
-console.log(bgHexCodeSpanElement);
